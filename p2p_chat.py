c65583282a9f7ac61ac752cf5d478bfe7334360b
Unknown <franizus@live.com>
2017-12-13 11:52:23 -0500

    added client and server code to p2p chat

--- p2p_chat.py
+++ p2p_chat.py
@@ -1,6 +1,9 @@
 import socket
 import subprocess
 import shlex
+import threading
+import select
+import sys
 import xml.etree.ElementTree as elt
 
 
@@ -17,23 +20,85 @@ def get_network_mask():
 
 
 def get_connected_peers():
-    args = shlex.split('nmap -oX ./ex.xml -p ' + str(PORT) + ' ' + get_network_mask() + ' --open')
+    args = shlex.split('nmap -oX ./ex.xml -p ' + str(PORT) +
+                       ' ' + get_network_mask() + ' --open')
     subprocess.call(args)
     xml_content = elt.parse('ex.xml').getroot()
     for address in xml_content.iter('address'):
         PEERS_LIST.append(address.get('addr'))
 
-def search_peers():
-    get_connected_peers()
-    if PEERS_LIST:
-        return True
-    else:
-        return False
+
+def prompt():
+    """Prints a prompt to the console to send messages."""
+    sys.stdout.write('>>> ')
+    sys.stdout.flush()
+
+
+def client_thread(peer_ip):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((peer_ip, PORT))
+    except socket.error as msg:
+        print('Connection failed. Error Code : ' +
+              str(msg[0]) + ' Message ' + msg[1])
+
+    socket_list = [sys.stdin, client_socket]
+    while True:
+        read_sockets, write_sockets, error_sockets = select.select(
+            socket_list, [], [])
+        for sock in read_sockets:
+            if sock != client_socket:
+                msg = sys.stdin.readline()
+                client_socket.send(msg.encode('ascii'))
+                prompt()
+
+    client_socket.close()
+
+
+def listen_client(client_socket):
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                print('\nDesconectado del peer.')
+            else:
+                sys.stdout.write(data.decode('ascii'))
+                prompt()
+        except socket.error:
+            client_socket.close()
+
+
+def server_thread():
+    print('Servidor Iniciado')
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.bind((HOST, PORT))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' +
+              str(msg[0]) + ' Message ' + msg[1])
+    server_socket.listen(10)
+
+    while 1:
+        connection, address = server_socket.accept()
+        if address[0] not in PEERS_LIST:
+            CLIENT_THREADS.append(threading.Thread(
+                target=client_thread, args=(address[0], )).start())
+        CONNECTION_THREADS.append(threading.Thread(
+            target=listen_client, args=(connection,)).start())
+
+    server_socket.close()
 
 
 if __name__ == "__main__":
+    HOST = ''
     PORT = 8889
     PEERS_LIST = []
-    print(search_peers())
-    print(PEERS_LIST)
-    print('fin')
+    CONNECTION_THREADS = []
+    CLIENT_THREADS = []
+
+    get_connected_peers()
+    threading.Thread(target=server_thread).start()
+    if PEERS_LIST:
+        for peer in PEERS_LIST:
+            CLIENT_THREADS.append(threading.Thread(
+                target=client_thread, args=(peer, )).start())
